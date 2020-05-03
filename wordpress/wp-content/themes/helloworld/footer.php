637deb195402deded91acd0ad90911c634ded6ab
leocavalcante <lc@leocavalcante.com>
2020-05-03 00:37:21 -0300

    Fix template files

---
+++ wordpress/wp-content/themes/helloworld/footer.php
@@ -0,0 +1,6 @@
+  <footer>
+    &copy; Learning WordPress
+  </footer>
+  <?php wp_footer() ?>
+</body>
+</html>
--- wordpress/wp-content/themes/helloworld/index.php
+++ wordpress/wp-content/themes/helloworld/index.php
@@ -1,16 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Learning WordPress</title>
-  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css">
-</head>
-<body>
-  <main>
-    <p>Hello, World!</p>
-  </main>
-</body>
-</html>
+<?php get_header() ?>
+<main>
+  <p>Hello, World!</p>
+</main>
+<?php get_footer() ?>
